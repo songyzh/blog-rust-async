403c54491a10db641c1ea4ca15f383dc96168dd7
songyzh <mailsyzh@gmail.com>
2020-02-08 21:41:27 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,11 @@ async fn main() {
 
     let mut server = tide::with_state(pool);
 
-    server.at("/benchmark").get(service::benchmark);
     server.at("/api").nest(|server| {
         server.at("/posts").get(service::get_posts);
         server.at("/posts/:slug").get(service::get_post_by_slug);
         server.at("/tags").get(service::get_tags);
+        server.at("/benchmark").get(service::benchmark);
     });
 
     server.listen(("127.0.0.1", 3333)).await.unwrap();
